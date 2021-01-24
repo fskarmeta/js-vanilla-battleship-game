bdf9af1ee9338cb3ebcbe96722255155e19fe898
Fabian Skarmeta <fabian.skarmeta@gmail.com>
2021-01-24 04:26:42 -0300

    i have to expand the ships object to take count of each one inidivually

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ const userArray = [];
 const computerArray = [];
 // matrix size
 const matrixSize = 9;
-
+// Ships to pick actual position
+let shipsHorizontal = true;
+// is game over
+let gameOver = false;
+// current player
+let currentPlayer = "user";
 ////////////////////////// SHIPS OBJECTS //////////////////////////////
 const smallShipObj = {
   name: "Small-Ship",
@@ -38,8 +43,6 @@ createGameBoard(matrixSize, userGameboard, userArray);
 createGameBoard(matrixSize, computerGameboard, computerArray);
 
 //////////////////////////// ROTATE SHIPS /////////////////////////
-// Ships to pick actual position
-let shipsHorizontal = true;
 
 // Rotate Ships to drag
 function rotateShips() {
@@ -221,3 +224,181 @@ generateRandomShip(smallShipObj);
 generateRandomShip(mediumShipObj);
 generateRandomShip(mediumShipObj);
 generateRandomShip(bigShipObj);
+
+/////////// GAME //////////////
+
+function play() {
+  console.log(currentPlayer);
+  if (gameOver) {
+    return;
+  }
+  if (currentPlayer == "user") {
+    turn.textContent = "It's your turn";
+    computerArray.forEach((square) =>
+      square.addEventListener("click", function (event) {
+        showSquare(square);
+      })
+    );
+  }
+  if (currentPlayer == "computer") {
+    turn.textContent = "Computer's turn";
+    setTimeout(() => {
+      computerPlays();
+    }, 800);
+  }
+}
+
+startButton.addEventListener("click", () => play());
+
+let smallShipCount = 0;
+let mediumShipCount = 0;
+let mediumShip2Count = 0;
+let bigShipCount = 0;
+let bigShip2Count = 0;
+
+function showSquare(square) {
+  if (!square.classList.contains("hit")) {
+    if (square.classList.contains("small-ship-0")) {
+      console.log("SI!");
+      smallShipCount++;
+    }
+    if (square.classList.contains("medium-ship-0")) {
+      mediumShipCount++;
+    }
+    if (square.classList.contains("medium-ship-1")) {
+      mediumShip2Count++;
+    }
+    if (square.classList.contains("big-ship-0")) {
+      bigShipCount++;
+    }
+    if (square.classList.contains("big-ship-1")) {
+      bigShip2Count++;
+    }
+  }
+  if (square.classList.contains("taken")) {
+    square.classList.add("hit");
+    console.log("click");
+  } else {
+    square.classList.add("miss");
+  }
+  checkWinner();
+  currentPlayer = "computer";
+  play();
+}
+
+let pcSmallShipCount = 0;
+let pcMediumShipCount = 0;
+let pcMediumShip2Count = 0;
+let pcBigShipCount = 0;
+let pcBigShip2Count = 0;
+
+const computerPlays = () => {
+  console.log("computer playing");
+  const randomSquare = Math.floor(Math.random() * userArray.length);
+  if (!userArray[randomSquare].classList.contains("hit")) {
+    userArray[randomSquare].classList.add("hit");
+    console.log(randomSquare);
+    if (userArray[randomSquare].classList.contains("small-ship-0")) {
+      pcSmallShipCount++;
+    }
+    if (userArray[randomSquare].classList.contains("medium-ship-0")) {
+      pcMediumShipCount++;
+    }
+    if (userArray[randomSquare].classList.contains("medium-ship-1")) {
+      pcMediumShip2Count++;
+    }
+    if (userArray[randomSquare].classList.contains("big-ship-0")) {
+      pcBigShipCount++;
+    }
+    if (userArray[randomSquare].classList.contains("big-ship-1")) {
+      pcBigShip2Count++;
+    }
+  } else {
+    computerPlays();
+  }
+  checkWinner();
+  currentPlayer = "user";
+  turn.textContent = "Your turn";
+};
+
+const checkWinner = () => {
+  console.log(`pc Small Ship ${pcSmallShipCount}`);
+  console.log(`pc Medium Ship ${pcMediumShipCount}`);
+  console.log(`pc Medium Ship 2 ${pcMediumShip2Count}`);
+  console.log(`pc Big Ship ${pcBigShipCount}`);
+  console.log(`pc Big Ship 2 ${pcBigShip2Count}`);
+
+  console.log(`user Small Ship ${smallShipCount}`);
+  console.log(`user Medium Ship ${mediumShipCount}`);
+  console.log(`user Medium Ship 2 ${mediumShip2Count}`);
+  console.log(`user Big Ship ${bigShipCount}`);
+  console.log(`user Big Ship 2 ${bigShip2Count}`);
+
+  if (smallShipCount === 2) {
+    generalInfo.textContent = "You sunk a computers small ship!";
+    smallShipCount = 10;
+  }
+  if (mediumShipCount === 3) {
+    generalInfo.textContent = "You sunk a computers medium sized ship!";
+    mediumShipCount = 10;
+  }
+  if (mediumShip2Count === 3) {
+    generalInfo.textContent = "You sunk a computers medium sized ship!";
+    mediumShip2Count = 10;
+  }
+  if (bigShipCount === 4) {
+    generalInfo = "You sunk a computers big ship!";
+    bigShipCount = 10;
+  }
+  if (bigShip2Count === 4) {
+    generalInfo = "You sunk a computers big ship!";
+    bigShip2Count = 10;
+  }
+  if (pcSmallShipCount === 2) {
+    generalInfo.textContent = "The computer sunk a small ship!";
+    pcSmallShipCount = 10;
+  }
+  if (pcMediumShipCount === 3) {
+    generalInfo.textContent = "The computer sunk a medium sized ship!";
+    pcrMediumShipCount = 10;
+  }
+  if (pcMediumShip2Count === 3) {
+    generalInfo.textContent = "The computer sunk medium sized ship!";
+    pcMediumShip2Count = 10;
+  }
+  if (pcBigShipCount === 4) {
+    generalInfo = "The computer sunk a computers big ship!";
+    pcBigShipCount = 10;
+  }
+  if (pcBigShip2Count === 4) {
+    generalInfo = "The computer a computers big ship!";
+    pcBigShip2Count = 10;
+  }
+  if (
+    smallShipCount +
+      mediumShipCount +
+      mediumShip2Count +
+      bigShipCount +
+      bigShip2Count ===
+    50
+  ) {
+    generalInfo.innerHTML = "YOU ARE THE WINNER !";
+    gameIsOver();
+  }
+  if (
+    pcSmallShipCount +
+      pcMediumShipCount +
+      pcMediumShip2Count +
+      pcBigShipCount +
+      pcBigShip2Count ===
+    50
+  ) {
+    generalInfo.innerHTML = "THE COMPUTER HAS WON.";
+    gameIsOver();
+  }
+};
+
+function gameIsOver() {
+  gameOver = true;
+  startButton.removeEventListener("click", play);
+}
